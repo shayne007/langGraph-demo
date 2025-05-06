e3c95e540c5d5a129a555bd8823215b9967ca181
shayne007 <shayne007@sina.cn>
2025-05-06 22:11:46 +0800

    human in the loop

---
+++ human_interrupt/README.md
@@ -0,0 +1,7 @@
+this demo showcase how to handle human-in-the-loop interruptions in a task-oriented dialogue system.
+To run the demo, follow the steps below:
+
+to know more details, see:
+https://langchain-ai.github.io/langgraph/concepts/human_in_the_loop/
+
+
--- human_interrupt/chat_with_human_interrupt.py
+++ human_interrupt/chat_with_human_interrupt.py
@@ -1,6 +1,6 @@
 from typing import Annotated
 
-from langchain_openai import ChatOpenAI 
+from langchain_openai import ChatOpenAI
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_core.tools import tool
 from typing_extensions import TypedDict
@@ -16,8 +16,9 @@ from dotenv import load_dotenv
 # Load environment variables from .env
 load_dotenv()
 
+
 class State(TypedDict):
-    messages: Annotated[list, add_messages]
+  messages: Annotated[list, add_messages]
 
 
 graph_builder = StateGraph(State)
@@ -25,15 +26,15 @@ graph_builder = StateGraph(State)
 
 @tool
 def human_assistance(query: str) -> str:
-    """Request assistance from a human."""
-    human_response = interrupt({"query": query})
-    return human_response["data"]
+  """Request assistance from a human."""
+  human_response = interrupt({"query": query})
+  return human_response["data"]
 
 
 tool = TavilySearchResults(max_results=2)
 tools = [tool, human_assistance]
 llm = ChatOpenAI(
-    model="deepseek-chat",  
+    model="deepseek-chat",
     base_url="https://api.deepseek.com",
     api_key=os.getenv("DEEPSEEK_API_KEY"),
     temperature=0.0
@@ -42,9 +43,9 @@ llm_with_tools = llm.bind_tools(tools)
 
 
 def chatbot(state: State):
-    message = llm_with_tools.invoke(state["messages"])
-    assert(len(message.tool_calls) <= 1)
-    return {"messages": [message]}
+  message = llm_with_tools.invoke(state["messages"])
+  assert (len(message.tool_calls) <= 1)
+  return {"messages": [message]}
 
 
 graph_builder.add_node("chatbot", chatbot)
@@ -61,3 +62,27 @@ graph_builder.add_edge(START, "chatbot")
 
 memory = MemorySaver()
 graph = graph_builder.compile(checkpointer=memory)
+
+user_input = "I need some expert guidance for building an AI agent. Could you request assistance for me?"
+config = {"configurable": {"thread_id": "1"}}
+
+events = graph.stream(
+    {"messages": [{"role": "user", "content": user_input}]},
+    config,
+    stream_mode="values",
+)
+for event in events:
+  if "messages" in event:
+    event["messages"][-1].pretty_print()
+
+human_response = (
+    "We, the experts are here to help! We'd recommend you check out LangGraph to build your agent."
+    " It's much more reliable and extensible than simple autonomous agents."
+)
+
+human_command = Command(resume={"data": human_response})
+
+events = graph.stream(human_command, config, stream_mode="values")
+for event in events:
+    if "messages" in event:
+        event["messages"][-1].pretty_print()
